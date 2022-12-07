85508ad2c7cca4ee01ddf6c5d812f295ada3cc74
Abhishek kumar <abhishekzadav@Gmail.com>
2022-12-07 10:19:57 +0530

    Assignment- HTML to React

---
+++ block-BRaaav/code/hydro/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./components/Header";
+import "../src/index.css"
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+
+function App(){
+    return (
+        <>
+        <Header/>
+        <Hero/>
+        <About/>
+        <Blog/>
+        <Work/>
+        <Contact/>
+        <Footer/>
+        </>
+    )
+}
+
+export default App
